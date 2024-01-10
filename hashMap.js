01f71b582294c200297c8bbf6e6856cb18ed2e7b
Andre <123985988+derre3@users.noreply.github.com>
2024-01-10 10:58:33 -0300

    refactor bucket structure

--- hashMap.js
+++ hashMap.js
@@ -1,28 +1,8 @@
 function HashMap() {
-  const loadFactor = 0.75;
+  const LOADFACTOR = 0.75;
   let size = 0;
   let bucketSize = 16;
-  let bucket = new Array(bucketSize);
-
-  const getBucket = () => bucket;
-  const getBucketSize = () => bucketSize;
-
-  const growBucketSize = () => {
-    const oldBucket = bucket;
-    size = 0;
-    bucketSize *= 2;
-    bucket = new Array(bucketSize);
-    oldBucket.forEach((node) => {
-      if (node === undefined) return;
-      else {
-        let cur = node.head;
-        while (cur) {
-          set(cur.key, cur.value);
-          cur = cur.next;
-        }
-      }
-    });
-  };
+  let bucket = new Array(bucketSize).fill([]); // fill with empty array to make use of the arr.flat()
 
   const hash = (value) => {
     let hashCode = 0;
@@ -36,45 +16,50 @@ function HashMap() {
   const set = (key, value) => {
     const hashCode = hash(key);
     const node = { key, value, next: null };
-    if (bucket[hashCode] === undefined) {
+    if (bucket[hashCode].length === 0) {
       bucket[hashCode] = {
         head: node,
       };
       size += 1;
     } else {
       let cur = bucket[hashCode].head;
-      if (cur.key === key) return (cur.value = value);
+      // if key is already present,
+      if (cur.key === key) return (cur.value = value); // overwrite value
+      // if key not present,
       while (cur.next) {
+        // traverse linked list until empty space is found
         cur = cur.next;
       }
       cur.next = node;
     }
-    if (getBucket().flat().length / getBucketSize() >= loadFactor)
-      growBucketSize();
+    if (bucket.flat().length / bucketSize >= LOADFACTOR) growBucketSize();
+  };
+
+  const growBucketSize = () => {
+    const oldBucket = bucket.flat(); // remove empty arrays
+    size = 0;
+    bucketSize *= 2;
+    bucket = new Array(bucketSize).fill([]);
+    oldBucket.forEach((node) => {
+      let cur = node.head;
+      while (cur) {
+        set(cur.key, cur.value);
+        cur = cur.next;
+      }
+    });
   };
 
   const get = (key) => {
     const hashCode = hash(key);
-    const bucket = getBucket();
-    if (!bucket[hashCode]) return null;
+    if (bucket[hashCode].length === 0) return null;
     let cur = bucket[hashCode].head;
     while (cur) {
+      // if key is not found: cur = null
+      // if key is found: cur = node
       if (cur.key === key) break;
       cur = cur.next;
     }
     return cur;
-  };
-
-  const has = (key) => {
-    const hashCode = hash(key);
-    const bucket = getBucket();
-    if (!bucket[hashCode]) return false;
-    let cur = bucket[hashCode].head;
-    while (cur) {
-      if (cur.key === key) break;
-      cur = cur.next;
-    }
-    return cur ? true : false;
   };
 
   const has = (key) => (get(key) ? true : false);
@@ -82,12 +67,11 @@ function HashMap() {
   const remove = (key) => {
     if (!has(key)) return;
     const hashCode = hash(key);
-    const bucket = getBucket();
     let prev;
     let cur = bucket[hashCode].head;
     size -= 1;
     if (cur.key === key) {
-      if (cur.next === null) return (bucket[hashCode] = undefined);
+      if (cur.next === null) return (bucket[hashCode] = []);
       else return (bucket[hashCode].head = cur.next);
     }
     while (cur) {
@@ -105,14 +89,12 @@ function HashMap() {
   const clear = () => {
     size = 0;
     bucketSize = 16;
-    bucket = new Array(bucketSize);
+    bucket = new Array(bucketSize).fill([]);
   };
 
   const keys = () => {
     let keysArr = [];
-    const bucket = getBucket();
-    bucket.forEach((node) => {
-      if (node === undefined) return;
+    bucket.flat().forEach((node) => {
       let cur = node.head;
       while (cur) {
         keysArr.push(cur.key);
@@ -124,9 +106,7 @@ function HashMap() {
 
   const values = () => {
     let valuesArr = [];
-    const bucket = getBucket();
-    bucket.forEach((node) => {
-      if (node === undefined) return;
+    bucket.flat().forEach((node) => {
       let cur = node.head;
       while (cur) {
         valuesArr.push(cur.value);
@@ -138,9 +118,7 @@ function HashMap() {
 
   const entries = () => {
     let entriesArr = [];
-    const bucket = getBucket();
-    bucket.forEach((node) => {
-      if (node === undefined) return;
+    bucket.flat().forEach((node) => {
       let cur = node.head;
       while (cur) {
         entriesArr.push([cur.key, cur.value]);
@@ -160,6 +138,29 @@ function HashMap() {
     keys,
     values,
     entries,
-    getBucket,
   };
 }
+
+const testHash = HashMap();
+const a = 'Andre';
+const b = 'Co';
+const c = 'Sther';
+const d = 'Cr';
+const w = 'TEST';
+
+testHash.set('a', a);
+testHash.set('b', b);
+testHash.set('c', c);
+testHash.set('d', d);
+testHash.set('w', w);
+testHash.remove('w');
+testHash.set('w', w);
+
+console.log(testHash.length()); // 5
+console.log(testHash.keys()); // ['a','b','c','d','w']
+console.log(testHash.values()); // ['Andre','Co','Sther','Cr','TEST']
+console.log(testHash.entries()); // [['a', 'Andre'], ['b', 'Co'], ['c', 'Sther'], ['d', 'Cr'], ['w', 'TEST']]
+console.log(testHash.get('w')); // { key: 'w', value: 'TEST', next: null }
+console.log(testHash.has('a')); // true
+console.log(testHash.get('z')); // null
+console.log(testHash.has('z')); // false
